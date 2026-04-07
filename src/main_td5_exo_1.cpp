54ca7efd9e47547b3ca70099486bbdfcef11e3ec
Yanis-Wg <yan.nw.77185@gmail.com>
2026-04-07 10:59:29 +0200

    TD5 - exo 1 - DONE

--- src/main_td5_exo_1.cpp
+++ src/main_td5_exo_1.cpp
@@ -2,9 +2,29 @@
 #include <string>
 #include <vector>
 
+// exercice 1
+size_t folding_string_hash(std::string const& s, size_t max){
+    size_t hash(0);
+    for (size_t i {0}; i<s.size(); i++)
+    {
+        // hash+=s[i];
+        // hash%=max;
+        // je fais les sommes asci en faisant le module de la somme à chaque tour pour éviter que ca sorte de la boucle
+        hash=(hash+s[i])%max;
+
+    }
+    
+    return hash;
+}
+
+// exercice 2 
+
 
 int main(){
-
+    std::string str{0};
+    std::cout <<"Entre une string : ";
+    std::cin>>str;
     return 0;
+
 }
 
